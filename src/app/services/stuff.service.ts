acecd2852e18a11f1370872357a7c25fb1bdbffc
skorikovkr <skorikovkr@gmail.com>
2023-01-26 14:32:23 +0500

    компонент товаров с имитацией бд

---
+++ src/app/services/stuff.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Stuff } from '../models/stuff';
+import { StuffCategory } from '../models/stuff-category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StuffService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllServices() : Observable<Stuff[]> {
+    let result = [
+      {id: 1, name: 'Шампунь для собаки', categoryId: 1, categoryName: ''}, 
+      {id: 2, name: 'Куртка для кошки', categoryId: 2, categoryName: ''},
+      {id: 3, name: 'Нахвостник шерстяной', categoryId: 2, categoryName: ''},
+      {id: 4, name: 'Тапочки для котят', categoryId: 2, categoryName: ''}
+    ];
+    return new Observable((subscriber) => {
+      subscriber.next(result);
+    });
+  }
+
+  getStuffByCategory(category: StuffCategory) : Observable<Stuff[]> {
+    let result : Stuff[] = [];
+    this.getAllServices()
+    .subscribe({
+        next: (stuff) => {
+          result = stuff.copyWithin(0, 0).filter(s => s.categoryId === category.id);
+        },
+        error: (response) => {
+          console.log(response);
+          return;
+        }
+      }
+    );
+    return new Observable((subscriber) => {
+      subscriber.next(result);
+    });
+  }
+}
